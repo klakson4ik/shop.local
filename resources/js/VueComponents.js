057e01544dd784c97a14caf244ea6f4947fb2475
Maksim <makszona@mail.ru>
2020-06-22 20:08:31 +0300

    work auth

--- resources/js/VueComponents.js
+++ resources/js/VueComponents.js
@@ -28,3 +28,7 @@ Vue.component('dropdown-component', require('./components/reusedComponents/Dropd
 Vue.component('button-component', require('./components/reusedComponents/ButtonComponent.vue').default)
 Vue.component('search-component', require('./components/reusedComponents/SearchComponent.vue').default)
 
+//User
+Vue.component('user-index-component', require('./components/User/UserIndexComponent.vue').default)
+
+
---
+++ resources/js/components/User/UserIndexComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>1111111111111</p>
+</template>
+
+<script>
+    export default {
+        name: "UserIndexComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
